908ff670e874b500a04c57e1ffc886d6531c7f05
motasimmakki <makkimotasim@gmail.com>
2025-04-09 00:41:32 +0530

    fix: detect function defination :wrench:

--- vehicle-detection.py
+++ vehicle-detection.py
@@ -44,7 +44,8 @@ if __name__ == '__main__':
 
 			bname = basename(splitext(img_path)[0])
 
-			R,_ = detect(vehicle_net, vehicle_meta, img_path ,thresh=vehicle_threshold)
+			# R,_ = detect(vehicle_net, vehicle_meta, img_path ,thresh=vehicle_threshold)
+			R, _ = detect(vehicle_net, vehicle_meta, img_path.encode('utf-8'), thresh=vehicle_threshold)
 
 			R = [r for r in R if r[0] in ['car','bus']]
 
